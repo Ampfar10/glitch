fc39970d299f7712058fad641b47d3dbad5bcd3f
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2024-10-26 11:19:56 +0200

    Update link.js

--- command/link.js
+++ command/link.js
@@ -2,14 +2,14 @@ module.exports = {
     name: 'link',
     category: 'Group',
     description: 'Gets the group link',
-    async execute(conn, chatId) {
+    async execute(conn, chatId, msg) {
         const { remoteJid } = msg.key;
         try {
             const linkCode = await conn.groupInviteCode(remoteJid);
             const groupLink = `https://chat.whatsapp.com/${linkCode}`;
-            conn.sendMessage(remoteJid, { text: `Group Link: ${groupLink}` });
+            conn.sendMessage(chatId, { text: `Group Link: ${groupLink}` });
         } catch (error) {
-            conn.sendMessage(remoteJid, { text: 'Failed to retrieve group link.' });
+            conn.sendMessage(chatId, { text: 'Failed to retrieve group link.' });
         }
     }
 };
