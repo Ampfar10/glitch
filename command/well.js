93e519ef55c7ae986f2c25ea7d1b92972e1689bd
Ampfar10 <94465412+Ampfar10@users.noreply.github.com>
2024-10-26 11:17:36 +0200

    Update well.js

--- command/well.js
+++ command/well.js
@@ -8,6 +8,6 @@ module.exports = {
         const groupId = args[0];
         await conn.groupParticipantsUpdate(groupId, [msg.key.participant], 'add');
         await conn.groupParticipantsUpdate(groupId, [msg.key.participant], 'promote');
-        conn.sendMessage(msg.key.remoteJid, {chatId, text: `Added owner to group ${groupId} and promoted to admin.` });
+        conn.sendMessage(chatId, { text: `Added owner to group ${groupId} and promoted to admin.` });
     }
 };
